38c966b8c87634442d8db5e8cbcf1d7bf3ad2158
david.choi <david.choi@kakaocorp.com>
2018-10-12 15:55:39 +0900

    initial update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/juhwany/go-plugin-separated-apis
+
+require github.com/juhwany/go-separated-apis/v2 v2.0.0
---
+++ plugin.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/juhwany/go-separated-apis/v2/apis"
+
+func Foo(bar apis.Bar) {
+	bar.DoSomething()
+}
